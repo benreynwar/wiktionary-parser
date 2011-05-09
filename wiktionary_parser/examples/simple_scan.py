bb223fad7d65571605264527acffb9f1d9ce0c3f
Ben Reynwar <ben@reynwar.net>
2011-05-08 21:21:51 -0700

    simple.wiktionary.org: Improved examples.

--- wiktionary_parser/examples/simple_scan.py
+++ wiktionary_parser/examples/simple_scan.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-This example extracts a number of words from the simple.wiktionary xml file.
+This example scans through the simple-english wiktionary file and displays
+an alerts that the parser raises.
 """
 
 from wiktionary_parser.xml_parser import XMLPageParser
@@ -9,21 +10,15 @@ from wiktionary_parser.languages.simple.page import simplePage
 xml_file = open('../../wiktionary_data/simplewiktionary-20110505-pages-articles.xml')
 xml_parser = XMLPageParser(xml_file, simplePage)
 
-# The words we want to extract
-wanted_words = set([u'fox', u'robust'])
+page_count = 0
+errors = 0
 
-found_words = set([])
-
-for title, page in xml_parser.from_titles(wanted_words):
+for page in xml_parser:
+    page_count += 1
     page.parse()
-    # Print out a summary of the want
-    for word in page.words:
-        print word.summary()
-        print('')
-    # Display any alerts that the parser raised.
-    # Alerts indicate bugs in the parser or errors on the wiktionary page.
     all_alerts = page.get_alerts()
     if all_alerts:
+        errors += 1
         print('--------')
         print(page.title)
         print('--------')        
@@ -31,4 +26,8 @@ for title, page in xml_parser.from_titles(wanted_words):
             print(alert.description)
             if hasattr(alert, 'message'):
                 print(alert.message)
-    
+
+    if page_count % 1000 == 0:
+        print('*************************')
+        print('%s pages parsed - %s pages with alerts' % (page_count, errors))
+        print('*************************')
